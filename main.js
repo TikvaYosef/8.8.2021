d72c84100db7e42c11f9f40771375aea3c058de2
TikvaYosef <tikvayosef13@gmail.com>
2021-08-09 15:05:15 +0300

    firstcommit

---
+++ main.js
@@ -0,0 +1,237 @@
+
+
+// פונקציות
+
+
+
+// שאלה 1
+
+// function myName() {
+//     var name = "tikva"
+//     for (var i = 0; i < 5; i++) {
+//         console.log(name)
+//     }
+// }
+// myName()
+
+
+
+// שאלה 2
+
+
+
+// function Name(firsName) {
+//     for (var i = 0; i < 5; i++) {
+//         console.log(firsName)
+//     }
+// }
+// Name("tikva")
+
+
+
+
+// שאלה 3
+
+// function sumNumbers(){
+//     var num1 = 2;
+//     var num2 = 2;
+//     console.log(num1+num2)
+// }
+// sumNumbers()
+
+
+
+// שאלה 4
+
+
+// function sumPrint(numbers){
+//     console.log(numbers)
+
+// }
+// sumPrint(2+2)
+
+
+
+// שאלה 5
+
+// function minimum(num1,num2){
+//     if (num1<num2){
+//         return(num1)
+//     }else{return(num2)}
+// }
+// var minReturn = minimum(3,2);
+// console.log(minReturn)
+
+
+
+// שאלה 6
+
+
+// function sum(number) {
+//     while (number > 0) {
+//         var modulu = number % 10;
+//         var number = (number / 10) - (modulu / 10);
+//         sumSum = modulu + modulu
+//         return (sumSum)
+//     }
+// }
+// var sumModulu = sum(123)
+// console.log(sumModulu)
+
+
+
+// שאלה 7
+
+
+// var maarach = [8, 5, 2, 4, 6, 3, 1];
+// sum = 0
+// function hibur(maarach) {
+//     for (var i = 0; i < maarach.length; i++) {
+//         sum = maarach[i] + sum
+//     }
+//     return (sum)
+// }
+// var x = hibur(maarach)
+// console.log(x)
+
+// var maarach = [8, 5, 2, 4, 6, 3, 1];
+// machpela = 1
+// function kefel(maarach) {
+//     for (var i = 0; i < maarach.length; i++) {
+//         machpela = maarach[i] * machpela
+
+//     }
+
+//     return (machpela)
+// }
+// var x = kefel(maarach)
+// console.log(x)
+
+
+
+
+// var maarach = [8, 5, 2, 4, 6, 3, 1];
+// minimum = maarach[0]
+// function min(maarach) {
+//     for (var i = 0; i < maarach.length; i++) {
+//         if (minimum > maarach[i]) {
+//             minimum=maarach[i]
+
+//         }
+
+//     }
+//     return (minimum)
+// }
+
+//     var x = min(maarach);
+//     console.log(x);
+
+
+
+
+// var maarach = [8, 5, 2, 4, 6, 3, 1];
+// maximum = maarach[0]
+// function max(maarach){
+//     for(var i = 0; i<maarach.length;i++){
+//         if(maximum<maarach[i]){
+//             maximum=maarach[i]
+//     }
+// }
+//     return(maximum)
+
+// }
+// var x = max(maarach)
+// console.log(x)
+
+
+// ???
+
+// var maarach = [8, 5, 2, 4, 6, 3, 1];
+
+// function pachot(maarach){
+//     for(var i = 0 ; i<maarach.length ; i++ ){
+//         minus = maarach[i]-2
+//         maarach = minus
+//     }
+//     return(maarach)
+// }
+// var x = pachot(maarach)
+// console.log(x)
+
+
+
+
+
+// שאלה 8
+
+// function machrozet(name1){
+
+//     return(name1.length)
+// }
+// var x = machrozet("tikva")
+// console.log(x)
+
+
+
+// שאלה 9
+
+// let maarach = [5, 4, 8, 3, 1, 6]
+// var userInput = Number(prompt("number"))
+
+
+// function maarachNum(maarach) {
+//     for (var i = 0; i < maarach.length; i++) {
+//         if (maarach[i] = userInput) {
+//             let index = i
+//             maarach.splice(index, i)
+//             maarach[i]
+//             console.log(maarach,i)
+//         } else {
+//             console.log("no exist")
+
+//         }
+   
+   
+
+//     }
+  
+    
+// }
+// maarachNum(maarach)
+
+
+
+
+
+
+
+
+// שאלה 10
+
+
+
+
+// var maarach = [2, 3, 6, 3, 1, 6,]
+// kefelNum = 1
+
+// function kefel(maarach) {
+//     for (var i = 0; i < maarach.length; i++) {
+//         maarach[i] = maarach[i]*3
+
+//     }
+//     return (maarach)
+// }
+// var x = kefel(maarach)
+// console.log(x)
+
+
+
+
+
+// שאלה 11
+
+
+var maarach = [6,8,6,4,8,5]
+
+
+
